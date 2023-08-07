e2b939ecfb2ce15931eeed1f9f650b39eeda36be
prince saspara <princesaspara46@gmail.com>
2023-08-07 13:45:33 +0530

    Add Feedback & Setting a Proxy

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,34 +1,41 @@
-import { createContext, useState,useEffect  } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([ ]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
 
-  useEffect(()=>{
-    fetchFeedback()
-  },[])
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
 
   //Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
-    const data = await response.json()
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
     setFeedback(data);
-    setIsLoading(false)
-  }
-
-  //add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+    setIsLoading(false);
   };
 
+  //add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+    
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
+  };
   //delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
@@ -61,7 +68,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback,
         feedbackEdit,
         updateFeedback,
-        isLoading
+        isLoading,
       }}
     >
       {children}
